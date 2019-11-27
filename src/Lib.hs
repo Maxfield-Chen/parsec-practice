ebce8e0504327ea7d347b966ecadd91b3ed177b5
maxfield-chen <maxfieldchen@gmail.com>
2019-11-26 16:26:47 -0800

    Changed to ghcide and refactored using sepBy and endBy

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,33 +2,13 @@ module Lib where
 
 import           Text.ParserCombinators.Parsec
 
-csvFile :: GenParser Char st [[String]]
-csvFile = do
-  result <- many line
-  _      <- eof
-  return result
-
-line :: GenParser Char st [String]
-line = do
-  result <- cells
-  _      <- eol
-  return result
-
-cells :: GenParser Char st [String]
-cells = do
-  first <- cellContent
-  next  <- remainingCells
-  return (first : next)
-
-remainingCells :: GenParser Char st [String]
-remainingCells = (char ',' >> cells) <|> return [] 
--- Note that shortcutting can occur with choice, goes left to right
-
-cellContent :: GenParser Char st String
-cellContent = many (noneOf ",\n")
-
-eol :: GenParser Char st Char
+csvFile = endBy line eol
+line = sepBy cell (char ',')
+cell = many (noneOf ",\n")
 eol = char '\n'
 
 parseCsv :: String -> Either ParseError [[String]]
 parseCsv = parse csvFile "(unknown)"
+
+someFunc :: IO ()
+someFunc = print "hello"
